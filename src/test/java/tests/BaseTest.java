b4f0c2d43116ba9ea988e08d1b218b66898755a4
Cosmina <cosmina.marina.ciobanu@gmail.com>
2023-06-26 21:52:52 +0300

    SwitchPage 26/06/2023

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,12 +5,15 @@ import org.testng.annotations.BeforeSuite;
 import pages.BasePage;
 import pages.RegisterPage.RegisterPage;
 import pages.SignInPage.SignInPage;
+import pages.SwitchToPage.SwitchToAlert;
 
 public class BaseTest{
 //se apeleaza clasa din sign in page, iti da acces in clasa pentru a putea apela clasa
 
     public SignInPage signInPage = SignInPage.getInstance();
     public RegisterPage registerPage = RegisterPage.getInstance();
+    public SwitchToAlert switchToAlertPage= SwitchToAlert.getInstance();
+
 
     @BeforeSuite
     public void init(){
